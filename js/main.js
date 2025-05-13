4bebe6e07dcaef1a0758974fa20f9fa38eb67482
sh0hruz2013 <shohruzruziyev04@gmail.com>
2025-05-13 16:23:46 +0500

    first commit

--- js/main.js
+++ js/main.js
@@ -22,3 +22,12 @@ const loadingDuration = 2000; // 2s
 setTimeout(() => {
   loading.classList.add("loading-none");
 }, loadingDuration);
+// link
+AOS.init();
+// Hamburger
+let btn = document.querySelector("#hamburger");
+let navMenu = document.querySelector(".nav-menu");
+let dark_btn = document.querySelector(".dark--btn");
+btn.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
